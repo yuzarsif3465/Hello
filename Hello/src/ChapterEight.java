2551fdf9bafa8d61f442468118cf2537d5745b86
Yusuf Zorlu <ysfzorlu.3465@gmail.com>
2025-03-04 11:10:44 +0300

    question 8.2 solved

--- Hello/src/ChapterEight.java
+++ Hello/src/ChapterEight.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ChapterEight {
     //8.1
-    public static void main(String[] args) {
+/*    public static void main(String[] args) {
         System.out.println("enter a 3 by 4 matrix row by row: ");
         Scanner input = new Scanner(System.in);
         double [][] matrix = new double[3][4];
@@ -22,7 +22,26 @@ public class ChapterEight {
         }
         return sum;
     }
+*/  //8.2
+    public static void main(String[] args) {
+        System.out.println("enter a 4 by 4 matrix row by row: ");
+        Scanner input = new Scanner(System.in);
+        double [][] matrix = new double[4][4];
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 4; col++) {
+                matrix [row] [col] = input.nextDouble();
+            }
+        }
+        System.out.println("sum of diagonal matrix: "+ sumMajorDiagonal(matrix));
 
+    }
+    public static double sumMajorDiagonal(double[][] m){
+        double sum = 0;
+        for (int row = 0; row < m.length; row++) {
+            sum += m[row][row];
+        }
+        return sum;
+    }
 
 
 }
